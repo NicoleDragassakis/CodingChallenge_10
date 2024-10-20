6c2c6a5689b9c8800a216c8c48eb18758990048f
Nicole Dragassakis <niki_dragasakis@icloud.com>
2024-10-20 18:54:41 -0400

    Handle stock availability and disable purchase button when out of stock - TASK THREE

--- main.js
+++ main.js
@@ -2,10 +2,21 @@
 
 //TASK TWO
 //adding event listener to selector dropdown
-
 const productSelector = document.getElementById('productSelector'); //references the HTML element though ID on javascrpit
 const productPrice = document.getElementById('price'); //references the HTML element though ID on javascrpit
 productSelector.addEventListener('change', function(){
 const selectedPrice = productSelector.value; //displays prices based on which product is selected an its matching value
 productPrice.textContent=`$${selectedPrice}`; 
+});
+
+//Task THREE
+const purchaseButton= document.getElementById('purchaseButton');
+productSelector.addEventListener('change', function() {
+    const selectedOption=productSelector.options[optionSelector.selectedIndex];
+    const stockStatus = selectedOption.getAttribute('data-stock');
+    if(stockStatus === 'in-stock'){
+        purchaseButton.disabled= false;
+    } else{
+        purchaseButton.disabled= true;
+    }
 });
