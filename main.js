595d5f35c8147465375d98f30abd9ad5e74be168
Nicole Dragassakis <niki_dragasakis@icloud.com>
2024-10-20 17:40:58 -0400

    Add event listener to update price on size selection - TASK TWO

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 //TASK ZERO BASIC SETUP
+
+//TASK TWO
+//adding event listener to selector dropdown
+
+const productSelector = document.getElementById('productSelector'); //references the HTML element though ID on javascrpit
+const productPrice = document.getElementById('price'); //references the HTML element though ID on javascrpit
+productSelector.addEventListener('change', function(){
+const selectedPrice = productSelector.value; //displays prices based on which product is selected an its matching value
+productPrice.textContent=`$${selectedPrice}`; 
+});
